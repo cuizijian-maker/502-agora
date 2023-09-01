6587b12cb0fdda2da1f5a0c51d6b53acdbbafd36
cuizijian <81254522@qq.com>
2023-09-01 22:20:25 +0800

    first initialize project

---
+++ src/main/java/com/agora/recording/ervice/AgoraRecordingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agora.recording.ervice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgoraRecordingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgoraRecordingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agora/recording/ervice/AgoraRecordingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agora.recording.ervice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgoraRecordingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
